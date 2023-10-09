3302ec8be2907085ec54e943cfb769a9ce433229
Flávio Félix <fau_felix30@hotmail.com>
2023-10-09 17:38:05 -0300

    Feito o primeiro dia

---
+++ src/test/java/br/com/flaviofelix/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flaviofelix.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
